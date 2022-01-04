fc25f70f1a4167c73d6c5a750ac7572deeaf023c
panosz <panosz@gmail.com>
2022-01-04 14:01:41 +0200

    neovim. keymap for :Telescope oldfiles

--- nvim/.config/nvim/lua/panos/keymaps.lua
+++ nvim/.config/nvim/lua/panos/keymaps.lua
@@ -90,6 +90,7 @@ keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 -- Telescope {{{
 -- keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
 keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
+keymap("n", "<leader>o", "<cmd>lua require'telescope.builtin'.oldfiles(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
 keymap("n", "<c-g>", "<cmd>Telescope live_grep<cr>", opts)
 
 -- }}}
