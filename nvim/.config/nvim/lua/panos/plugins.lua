9af969d6126b5bab539f75e7a0a438796210a71c
panosz <panosz@gmail.com>
2022-01-04 11:01:16 +0200

    neovim. add treesiter-playground plugin.

--- nvim/.config/nvim/lua/panos/plugins.lua
+++ nvim/.config/nvim/lua/panos/plugins.lua
@@ -119,6 +119,7 @@ return packer.startup(function(use)
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
   }
+  use "nvim-treesitter/playground"
   --}}}
 --{{{ devicons - add filetype signs to your plugins
   use {
