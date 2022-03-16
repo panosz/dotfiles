590a72088de207fd28f9233b5f35f8078800711d
panosz <panosz@gmail.com>
2022-03-16 14:50:53 +0200

    nvim. added abbreviation

--- nvim/.config/nvim/lua/panos/abbreviations.lua
+++ nvim/.config/nvim/lua/panos/abbreviations.lua
@@ -1,4 +1,5 @@
 vim.cmd([[:iabbrev <expr> dts strftime('%Y-%m-%d %H:%M')]])
+vim.cmd([[:iabbrev <expr> rnds toupper(system("pwgen"))]])
 vim.cmd([[:iabbrev chck ]])
 vim.cmd([[:iabbrev unchck ]])
 vim.cmd([[:iabbrev blt 喇]])
