d0f08555fbfce516ce5d5a7ffab0b649c4c5c4fb
panosz <panosz@gmail.com>
2022-12-30 10:57:59 +0200

    nvim. add more null-ls sources.

--- nvim/.config/nvim/lua/panos/lsp/mason_config.lua
+++ nvim/.config/nvim/lua/panos/lsp/mason_config.lua
@@ -43,7 +43,9 @@ local null_ls = require("null-ls")
 null_ls.setup({
     sources = {
        null_ls.builtins.diagnostics.flake8,
-       null_ls.builtins.diagnostics.chktex
+       null_ls.builtins.diagnostics.chktex,
+       null_ls.builtins.code_actions.refactoring,
+       null_ls.builtins.formatting.black,
      }
   }
 )
