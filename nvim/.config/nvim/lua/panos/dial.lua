6f4e126b0faf622b9ddaeac2217841a187d88be4
panosz <panosz@gmail.com>
2023-02-08 15:41:25 +0200

    nvim. dial plugin. config using new interface.

--- nvim/.config/nvim/lua/panos/dial.lua
+++ nvim/.config/nvim/lua/panos/dial.lua
@@ -1,16 +1,15 @@
-local status_ok, dial = pcall(require, "dial")
+local status_ok, augend = pcall(require, "dial.augend")
 if not status_ok then
-  return
+	return
 end
 
-local searchlist = {
-  "number#decimal#int", -- decimal integer including negative (0, 192, -3, etc.)
-  "date#[%Y-%m-%d]",    -- standard date %Y-%m-%d
-  "date#[%Y/%m/%d]",    -- standard date %Y/%m/%d
-  "date#[%H:%M:%S]",    -- standard time %H:%M:%S
-  "number#hex",         -- hex number (e.g. 0x3f)
-}
-
-dial.config.searchlist.normal = searchlist
-dial.config.searchlist.visual = searchlist
-
+require("dial.config").augends:register_group({
+	default = {
+		augend.integer.alias.decimal,
+		augend.date.alias["%m/%d"],
+		augend.date.alias["%Y-%m-%d"], -- standard date %Y-%m-%d
+		augend.date.alias["%Y/%m/%d"], -- standard date %Y/%m/%d
+		augend.date.alias["%H:%M:%S"], -- standard time %H:%M:%S
+		augend.date.alias["%H:%M"],
+	},
+})
