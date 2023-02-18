d2864d1b23b9f96e3fd67301027d35df1a3d2f79
panosz <panosz@gmail.com>
2023-02-18 23:59:08 +0200

    nvim. uninstall ThePrimeagen/refactoring.nvim

--- nvim/.config/nvim/lua/panos/plugins.lua
+++ nvim/.config/nvim/lua/panos/plugins.lua
@@ -180,14 +180,6 @@ return packer.startup(function(use)
 
   --}}}
 
-use { -- refactoring tool
-    "ThePrimeagen/refactoring.nvim",
-    requires = {
-        {"nvim-lua/plenary.nvim"},
-        {"nvim-treesitter/nvim-treesitter"}
-    }
-}
-
   -- snippets{{{
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
