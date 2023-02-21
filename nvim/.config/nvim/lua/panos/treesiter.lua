9f3cf44a4f36eaeca2825e8b811bddc7cdd81e18
panosz <panosz@gmail.com>
2023-02-21 13:56:39 +0200

    nvim. Install treesiter parser for markdown, but disable highlighting.
    -- treesiter highlighting does not work well with vimwiki.

--- nvim/.config/nvim/lua/panos/treesiter.lua
+++ nvim/.config/nvim/lua/panos/treesiter.lua
@@ -4,12 +4,12 @@ if not status_ok then
 end
 
 configs.setup {
-  ensure_installed ={"c", "cpp", "python", "html", "lua", "vue", "latex" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed ={"c", "cpp", "python", "html", "lua", "vue", "latex", "markdown" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = {}, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
-    disable = { "org" }, -- list of language that will be disabled
+    disable = { "org", "markdown" }, -- list of language that will be disabled
     additional_vim_regex_highlighting = true,
   },
   -- indent = { enable = true, disable = { "yaml" } },
