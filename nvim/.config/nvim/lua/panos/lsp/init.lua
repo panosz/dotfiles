42a3dc1cdf73af684625f19887e68b11544ce4e8
panosz <panosz@gmail.com>
2023-05-16 10:26:41 +0300

    nvim. lsp.init.lua. I don't really know what I've done here.

--- nvim/.config/nvim/lua/panos/lsp/init.lua
+++ nvim/.config/nvim/lua/panos/lsp/init.lua
@@ -1,15 +1,16 @@
 local neodev_status, neodev = pcall(require, "neodev")
 if neodev_status then
-  neodev.setup({})
+	neodev.setup({
+		library = { plugins = { "nvim-dap-ui" }, types = true },
+	})
 end
 
 local lspconfig_status, _ = pcall(require, "lspconfig")
 if not lspconfig_status then
-  return
+	return
 end
-
 
 require("panos.lsp.mason_config")
 require("panos.lsp.handlers").setup()
 -- Turn on lsp status information
-require('fidget').setup()
+require("fidget").setup()
