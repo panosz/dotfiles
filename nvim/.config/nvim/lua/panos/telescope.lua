d89a8f3afdd48047f0ef9f4f3a27b1ccad7140fe
panosz <panosz@gmail.com>
2023-10-25 13:29:31 +0300

    nvim. hacky solution for telescope opening file in insert mode
    - see https://github.com/nvim-telescope/telescope.nvim/issues/2501#issuecomment-1561838990

--- nvim/.config/nvim/lua/panos/telescope.lua
+++ nvim/.config/nvim/lua/panos/telescope.lua
@@ -207,3 +207,13 @@ telescope.load_extension('project')
 telescope.load_extension("frecency")
 telescope.load_extension("ui-select")
 telescope.load_extension("file_browser")
+
+
+-- hacky solution for telescope opening file in insert mode, see https://github.com/nvim-telescope/telescope.nvim/issues/2501#issuecomment-1561838990
+vim.api.nvim_create_autocmd("WinLeave", {
+  callback = function()
+    if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
+    end
+  end,
+})
