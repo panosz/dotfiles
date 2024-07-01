97d1c9f9569594c3e489a7a576ee40051cf1a4ea
panosz <panosz@gmail.com>
2024-07-01 10:47:01 +0300

    nvim. fixed deprecation warnings in gitsigns config.

--- nvim/.config/nvim/lua/panos/gitsigns_config.lua
+++ nvim/.config/nvim/lua/panos/gitsigns_config.lua
@@ -47,11 +47,12 @@ end
 
 config.setup{
       signs = {
-        add          = {hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-        change       = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-        delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-        topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-        changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+        add          = { text = '│'},
+        change       = { text = '│'},
+        delete       = { text = '_'},
+        changedelete = { text = '~'},
+        topdelete    = { text = '‾'},
+        untracked    = { text = '┆' }
       },
       signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
       numhl      = true, -- Toggle with `:Gitsigns toggle_numhl`
@@ -84,8 +85,5 @@ config.setup{
         relative = 'cursor',
         row = 0,
         col = 1
-      },
-      yadm = {
-        enable = false
       },
     }
