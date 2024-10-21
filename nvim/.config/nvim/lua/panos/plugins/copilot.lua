543a26def1ff7c713bdc305442f8c32ef6b3bad1
panosz <panosz@gmail.com>
2024-10-21 22:03:57 +0300

    nvim. moved copilot config in plugins.copilot

--- nvim/.config/nvim/lua/panos/plugins/copilot.lua
+++ nvim/.config/nvim/lua/panos/plugins/copilot.lua
@@ -4,7 +4,47 @@ return {
 		cmd = "Copilot",
 		event = "InsertEnter", -- this is the recommended way to lazy load copilot
 		config = function()
-			require("panos.copilot")
+			local copilot = require("copilot")
+			copilot.setup({
+				panel = {
+					enabled = false,
+					auto_refresh = false,
+					keymap = {
+						jump_prev = "[[",
+						jump_next = "]]",
+						accept = "<CR>",
+						refresh = "gr",
+						open = "<M-CR>",
+					},
+				},
+				suggestion = {
+					enabled = false,
+					auto_trigger = true,
+					debounce = 75,
+					keymap = {
+						accept = "<c-a>",
+						accept_word = false,
+						accept_line = false,
+						next = "<c-l>",
+						prev = "<c-h>",
+						dismiss = "<C-e>",
+					},
+				},
+				copilot_node_command = "node",
+				filetypes = {
+					yaml = false,
+					markdown = false,
+					help = false,
+					gitcommit = true,
+					gitrebase = false,
+					hgcommit = false,
+					vimwiki = false,
+					svn = false,
+					cvs = false,
+					["."] = false,
+				}, -- Node.js version must be > 18.x
+				server_opts_overrides = {},
+			})
 		end,
 	},
 	{
