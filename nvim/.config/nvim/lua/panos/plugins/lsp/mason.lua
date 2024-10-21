f7e51811467a3c3bd206d3fafc81e4e0bc4f5a38
panosz <panosz@gmail.com>
2024-10-21 14:21:35 +0300

    nvim. use prettier icons in mason ui.

--- nvim/.config/nvim/lua/panos/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/panos/plugins/lsp/mason.lua
@@ -1,18 +1,26 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = { "williamboman/mason-lspconfig.nvim" },
-	config = function()
-		local mason = require("mason")
+  "williamboman/mason.nvim",
+  dependencies = { "williamboman/mason-lspconfig.nvim" },
+  config = function()
+    local mason = require("mason")
 
-		local mason_lspconfig = require("mason-lspconfig")
+    local mason_lspconfig = require("mason-lspconfig")
 
-		local servers = require("panos.plugins.lsp.settings.servers")
-		mason.setup()
+    local servers = require("panos.plugins.lsp.settings.servers")
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗"
+        }
+      }
+    })
 
-		mason_lspconfig.setup({
-			ensure_installed = vim.tbl_keys(servers),
-            -- auto-install configured servers (with lspconfig)
-            automatic_installation = true,
-		})
-	end,
+    mason_lspconfig.setup({
+      ensure_installed = vim.tbl_keys(servers),
+      -- auto-install configured servers (with lspconfig)
+      automatic_installation = true,
+    })
+  end,
 }
