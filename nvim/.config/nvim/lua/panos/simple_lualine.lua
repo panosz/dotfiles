05d3275dde3d1f6686337cfeea8e7b7ccb10ecfa
panosz <panosz@gmail.com>
2024-10-30 22:54:20 +0200

    nvim. add more lualine extensions

--- nvim/.config/nvim/lua/panos/simple_lualine.lua
+++ nvim/.config/nvim/lua/panos/simple_lualine.lua
@@ -1,62 +1,62 @@
 local status_ok, lualine = pcall(require, "lualine")
 if not status_ok then
-  return
+	return
 end
 
-lualine.setup {
-  options = {
-    icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = '' },
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = true,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    }
-  },
-  sections = {
-    lualine_a = { 'mode' },
-    lualine_b = { 'branch', 'diff', 'diagnostics' },
-    lualine_c = {
-      {
-        'filename',
-        path = 1, -- 0: Just the filename
-        -- 1: Relative path
-        -- 2: Absolute path
+lualine.setup({
+	options = {
+		icons_enabled = true,
+		theme = "auto",
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
+		disabled_filetypes = {
+			statusline = {},
+			winbar = {},
+		},
+		ignore_focus = {},
+		always_divide_middle = true,
+		globalstatus = true,
+		refresh = {
+			statusline = 1000,
+			tabline = 1000,
+			winbar = 1000,
+		},
+	},
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = { "branch", "diff", "diagnostics" },
+		lualine_c = {
+			{
+				"filename",
+				path = 1, -- 0: Just the filename
+				-- 1: Relative path
+				-- 2: Absolute path
 
-        shorting_target = 60, -- Shortens path to leave 60 spaces in the window
-        -- for other components. (terrible name, any suggestions?)
-      },
+				shorting_target = 60, -- Shortens path to leave 60 spaces in the window
+				-- for other components. (terrible name, any suggestions?)
+			},
 
-      {  -- show macro recording in statusline
-        require("noice").api.status.mode.get,
-        cond = require("noice").api.status.mode.has,
-        color = { fg = "#ff9e64" },
-      },
-
-    },
-    lualine_x = { 'copilot', 'filetype' },
-    lualine_y = { 'aerial' },
-    lualine_z = { 'location', 'progress' }
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {'aerial', 'quickfix', 'fugitive', 'toggleterm' },
-}
+			{ -- show macro recording in statusline
+				require("noice").api.status.mode.get,
+				cond = require("noice").api.status.mode.has,
+				color = { fg = "#ff9e64" },
+			},
+		},
+		lualine_x = { "copilot", "filetype" },
+		lualine_y = { "aerial" },
+		lualine_z = { "location", "progress" },
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = {},
+		lualine_x = {},
+		lualine_y = {},
+		lualine_z = {},
+	},
+	tabline = {},
+	winbar = {},
+	inactive_winbar = {},
+	extensions = { "aerial", "fugitive", "lazy", "mason", "nvim-tree", "quickfix", "toggleterm",
+	},
+})
