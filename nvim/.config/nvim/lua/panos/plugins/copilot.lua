358a5a1f02c6b63a91d7d456b6d22b2a8445cca0
panosz <panosz@gmail.com>
2024-11-17 00:05:44 +0200

    nvim. added CopilotChat pluggin.

--- nvim/.config/nvim/lua/panos/plugins/copilot.lua
+++ nvim/.config/nvim/lua/panos/plugins/copilot.lua
@@ -54,4 +54,17 @@ return {
 		end,
 	},
 	{ "AndreM222/copilot-lualine" },
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "canary",
+		dependencies = {
+			{ "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+		},
+		build = "make tiktoken", -- Only on MacOS or Linux
+		opts = {
+			-- See Configuration section for options
+		},
+		-- See Commands section for default commands if you want to lazy load on them
+	},
 }
