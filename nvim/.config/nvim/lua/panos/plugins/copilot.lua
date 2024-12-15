8f03318415595392defb007fa0bed6863ff5a04b
panosz <panosz@gmail.com>
2024-12-15 18:40:56 +0200

    nvim. CopilotChat. select `main` branch, as `canary` is no longer recommended.

--- nvim/.config/nvim/lua/panos/plugins/copilot.lua
+++ nvim/.config/nvim/lua/panos/plugins/copilot.lua
@@ -56,7 +56,7 @@ return {
 	{ "AndreM222/copilot-lualine" },
 	{
 		"CopilotC-Nvim/CopilotChat.nvim",
-		branch = "canary",
+		branch = "main",
 		dependencies = {
 			{ "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
 			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
