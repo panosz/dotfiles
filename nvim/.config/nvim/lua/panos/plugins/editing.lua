6327951d18d582a45f605808d50681f5da70a704
panosz <panosz@gmail.com>
2024-12-23 17:15:27 +0200

    nvim. organize editing plugins.

---
+++ nvim/.config/nvim/lua/panos/plugins/editing.lua
@@ -0,0 +1,9 @@
+return {
+	{ "godlygeek/tabular" }, -- tabular.vim: Text aligning tools
+	{ "scrooloose/nerdcommenter" }, -- comment functions
+	{ "simnalamburt/vim-mundo" }, -- Mundo - visualize undo tree
+	{ "tpope/vim-surround" }, -- surround.vim: quoting/parenthesizing made simple
+	{ "tpope/vim-repeat" }, -- repeat.vim: Repeat plugin maps using the `.` command
+	{ "tpope/vim-abolish" }, -- abolish  - Tim pope's plugin for working with variants of a word
+	{ "windwp/nvim-autopairs" }, -- autopair pluggin that suppors multiple characters
+}
--- nvim/.config/nvim/lua/panos/plugins/plugins.lua
+++ nvim/.config/nvim/lua/panos/plugins/plugins.lua
@@ -3,19 +3,6 @@ return {
 	{ "anufrievroman/vim-angry-reviewer" },
 	{ "moll/vim-bbye" }, -- Buffer bye - delete buffers without closing windows
 
-	{ "scrooloose/nerdcommenter" }, -- comment functions
-
-	{ "simnalamburt/vim-mundo" }, -- Mundo - visualize undo tree
-	{ "tpope/vim-surround" }, -- surround.vim: quoting/parenthesizing made simple
-
-	{ "tpope/vim-repeat" }, -- repeat.vim: Repeat plugin maps using the `.` command
-
-	{ "godlygeek/tabular" }, -- tabular.vim: Text aligning tools
-
-	{ "tpope/vim-abolish" }, -- abolish  - Tim pope's plugin for working with variants of a word
-
-	{ "windwp/nvim-autopairs" }, -- autopair pluggin that suppors multiple characters
-
 	{ "Shatur/neovim-session-manager" }, -- neovim session manager
 
 	{ "akinsho/toggleterm.nvim" },
