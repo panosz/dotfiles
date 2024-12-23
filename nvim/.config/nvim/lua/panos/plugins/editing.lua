f3eaea7438ad4f86164271811a7c1ad40650f18d
panosz <panosz@gmail.com>
2024-12-23 18:07:23 +0200

    nvim. plugins. moved autopairs config to editing.lua.

--- nvim/.config/nvim/lua/panos/plugins/editing.lua
+++ nvim/.config/nvim/lua/panos/plugins/editing.lua
@@ -41,5 +41,37 @@ return {
 	{ "tpope/vim-surround" }, -- surround.vim: quoting/parenthesizing made simple
 	{ "tpope/vim-repeat" }, -- repeat.vim: Repeat plugin maps using the `.` command
 	{ "tpope/vim-abolish" }, -- abolish  - Tim pope's plugin for working with variants of a word
-	{ "windwp/nvim-autopairs" }, -- autopair pluggin that suppors multiple characters
+	{ -- autopair pluggin that suppors multiple characters
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup({
+				check_ts = true,
+				ts_config = { -- treesitter options
+					lua = { "string", "source" },
+					javascript = { "string", "template_string" },
+					java = false,
+				},
+				disable_filetype = { "TelescopePrompt", "spectre_panel" },
+				fast_wrap = {
+					map = "<M-e>",
+					chars = { "{", "[", "(", '"', "'" },
+					pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+					offset = 0, -- Offset from pattern match
+					end_key = "$",
+					keys = "qwertyuiopzxcvbnmasdfghjkl",
+					check_comma = true,
+					highlight = "PmenuSel",
+					highlight_grey = "LineNr",
+				},
+			})
+
+			-- automatically add parentheses at autocompleted functions.
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local cmp_status_ok, cmp = pcall(require, "cmp")
+			if not cmp_status_ok then
+				return
+			end
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
+		end,
+	},
 }
